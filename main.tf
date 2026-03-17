cfbffa5d58d17a3209553caf7fe5dd246129c013
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-17 07:00:33 -0500

    Add Azure virtual network and subnet resources

--- main.tf
+++ main.tf
@@ -31,3 +31,17 @@ resource "azurerm_role_assignment" "storage_reader" {
   role_definition_name = "Reader"
   principal_id         = var.principal_id
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-azure-rbac-lab"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-app"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
