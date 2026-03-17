adbb08ec4bec516dec39dae677987cb1c25fe44d
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-17 06:35:41 -0500

    Add Azure provider and storage account resource

--- main.tf
+++ main.tf
@@ -1,6 +1,27 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
-# Root Terraform configuration
-# Azure provider and resources will be added here.
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-azure-rbac-lab"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "abahstorage12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
